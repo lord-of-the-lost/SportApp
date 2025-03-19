5d11fed5464cae067d208a230097fb28da7ffdef
lord-of-the-lost <ignatov.99@yandex.ru>
2025-03-19 21:11:16 +0300

    Initial Commit

---
+++ StortApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StortApp
+//
+//  Created by Николай Игнатов on 19.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
